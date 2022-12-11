9fed29c99482eb0df9d5bef47dd333dea34de0eb
Denys.Sh <7860denis@gmail.com>
2022-12-11 14:51:43 +0200

    Initial commit, added properties for application

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wine4You
+![](images/sommelier.jpg)
 Wine library
+
+A virtual sommelier will help you choose an exquisite and extraordinary wine for any event.
---
+++ src/main/java/com/sommelier/wine4you/Wine4youApplication.java
@@ -0,0 +1,13 @@
+package com.sommelier.wine4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wine4youApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wine4youApplication.class, args);
+    }
+
+}
